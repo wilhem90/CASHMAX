a551d3dad8cc7d5bd16118af560b4bf1665ef272
Wilhem Wundt Maxime <wilhemmaxime97@gmail.com>
2024-05-18 22:49:17 -0300

    refresh

--- backend/middlleware/userMiddlleware.js
+++ backend/middlleware/userMiddlleware.js
@@ -1,17 +1,20 @@
 
 function verify(req, res, next) {
-    const login = false
+    const login = true
     if (login === true) {
         console.log("Logado com sucesso!");
         next()
     }else {
         console.log("Login required!");
-        res.status(401).send({
+        res.status(400).send({
             error: 1,
             message: "Login required!"
         })
     }
 }
+
+
+
 
 
 
--- frontend/requestHTTP.js
+++ frontend/requestHTTP.js
@@ -1,3 +1,5 @@
+alert(51+5)
+
 const users = document.querySelector('.users')
 async function getUser() {
     const http = await fetch('http://localhost:3333/users')
@@ -12,5 +14,23 @@ async function getUser() {
         });
     }
 }
+
+
+const accessToken = 'seu_access_token_aqui';
+
+fetch('http://localhost:3333/users/EXdUvowZrZHKDDtElSv8', {
+    method: 'DELETE',
+    headers: {
+        'Authorization': `Bearer ${accessToken}`,
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        title: 'Meu Novo Post',
+        content: 'Conteúdo do post'
+    })
+})
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(error => console.error('Erro:', error));
 
 getUser()
